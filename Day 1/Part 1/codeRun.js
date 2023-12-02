31dcf2e8273eadf844b043a7870427df85059a88
mimbi <dimabwhite98@gmail.com>
2023-12-02 17:09:49 +0000

    Part 1

---
+++ Day 1/Part 1/codeRun.js
@@ -0,0 +1,23 @@
+let data = require('./input')
+function countMagicNumber(data) {
+    let parsedData = data.split('\n');
+    let resultsArr = parsedData.map((singleRow) => {
+        let firstFound = false;
+        let firstInt;
+        let lastInt;
+        for(let i=0; i< singleRow.length; i++){
+            if(!isNaN(singleRow[i])){
+                if(!firstFound) {
+                    firstInt = singleRow[i];
+                    firstFound = true;
+                }
+                lastInt = singleRow[i];
+            }
+        }
+        return parseInt(firstInt + lastInt);
+    })
+    return resultsArr.reduce(function (result, singleElement) {
+        return result + singleElement;
+    });
+}
+console.log(countMagicNumber(data));
