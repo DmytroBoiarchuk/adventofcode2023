5ddf6fa79cc75196e0e10967eaa1109cc4d26ea3
mimbi <dimabwhite98@gmail.com>
2023-12-09 21:40:31 +0000

    Day 9

---
+++ Day 9/Part 2/Part 2.js
@@ -0,0 +1,50 @@
+const data = require('../input')
+const histories = data.split('\n').map(row => row.split(' ').map( z => parseInt(z)))
+let result = 0
+for(let i = 0 ; i < histories.length; i++) {
+    let forSum = [histories[i][0]]
+    let row = histories[i]
+    let tempoArr = []
+    for (let j = 0; j < row.length-1; j++) {
+        tempoArr.push(row[j+1] - row[j])
+    }
+    while (!tempoArr.every(el => el === 0)){
+        forSum.push(tempoArr[0])
+        tempoArr = main(tempoArr)
+    }
+    console.log(forSum)
+    result += forSum.reverse().reduce((a,c) => c - a)
+}
+
+console.log('Sum is: ' + result)
+function main(tempoArr) {
+    let newArr = []
+    for (let j = 0; j < tempoArr.length-1; j++) {
+        newArr.push(tempoArr[j+1] - tempoArr[j])
+    }
+    return newArr
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
